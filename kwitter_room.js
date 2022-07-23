70befa75feed014401b87af5f2e8f55ecc1e79a9
aryaman554 <95351160+aryaman554@users.noreply.github.com>
2022-07-23 17:43:05 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTfu-X_hz8ox-oBmXl4B5pMtBvlHE02eo",
+    authDomain: "lets-chat-web-app-e6a21.firebaseapp.com",
+    projectId: "lets-chat-web-app-e6a21",
+    storageBucket: "lets-chat-web-app-e6a21.appspot.com",
+    messagingSenderId: "191021702648",
+    appId: "1:191021702648:web:96965eddaeb71a40248a0d"
+  };
+  
+  // Initialize Firebase
+  firebase. initializeApp(firebaseConfig);
